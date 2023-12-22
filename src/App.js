19bf6e9b01d3ec0be288d96314327768645bcb35
Ashlay <105703068+AshlayMeijer@users.noreply.github.com>
2023-12-22 09:43:03 +0100

    Pushed

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// App.js
+import React from 'react';
+import './index.css';
+
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
+
